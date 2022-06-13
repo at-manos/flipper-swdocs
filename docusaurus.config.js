7880301345804f410155bc6e6d2978d4e55a6c16
at-manos <manosaiden@gmail.com>
2022-06-13 01:27:49 -0400

    fix: footer github link
    changed to repo link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -136,7 +136,7 @@ const config = {
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/at-manos/flipper-swdocs',
               },
             ],
           },
